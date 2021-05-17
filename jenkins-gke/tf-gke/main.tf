78e3744b3fcce752383935655612c28631846db6
gkumar208 <gaurav.kumar@dxc.com>
2021-05-17 08:41:27 +0000

    revoke project_services version

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -16,7 +16,7 @@ gcurl "https://serviceusage.googleapis.com/v1/projects/${PROJECT_NUMBER}/service
  *****************************************/
 module "enables-google-apis" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "10.1.1"
+  version = "6.0.0"
 
   project_id = var.project_id
 
