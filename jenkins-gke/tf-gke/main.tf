a307eca1da0921fe1fb75cfe2905c3753d9c34a3
gkumar208 <gaurav.kumar@dxc.com>
2021-06-07 14:59:03 +0000

    fix zones

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -117,7 +117,7 @@ module "jenkins-gke" {
 resource "null_resource" "get-credentials" {
  depends_on = [module.jenkins-gke.name] 
  provisioner "local-exec" {   
-   command = "gcloud container clusters get-credentials ${module.jenkins-gke.name} --zone=${element((var.zones), 1)}"   
+   command = "gcloud container clusters get-credentials ${module.jenkins-gke.name} --zone=${element(jsonencode(var.zones), 0)}"   
   }
 }
 
