5049e4ad8d2f2ba2792bd383434a3d738a277501
gkumar208 <gaurav.kumar@dxc.com>
2021-06-10 12:37:18 +0000

    add get-cred

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -191,12 +191,12 @@ resource "google_storage_bucket_iam_member" "tf-state-writer" {
   }
 
 #####--zone=${element(jsonencode(var.zones), 0)}" 
-#  resource "null_resource" "get-credentials" {
-#   #depends_on = [module.asm.cluster_name] 
-#   provisioner "local-exec" {   
-#     command = "gcloud container clusters get-credentials ${module.jenkins-gke.name} --zone=${var.region}"
-#    }
-#  }
+ resource "null_resource" "get-credentials" {
+  depends_on = [module.jenkins-gke.name] 
+  provisioner "local-exec" {   
+    command = "gcloud container clusters get-credentials ${module.jenkins-gke.name} --zone=${var.region}"
+   }
+ }
 
 data "local_file" "helm_chart_values" {
   filename = "${path.module}/values.yaml"
