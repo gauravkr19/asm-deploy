e6e4ad341907dee135187e4d788d777e6ed57167
gauravkr19 <gauravkr19@gmail.com>
2021-06-13 09:30:54 +0000

    machines

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -99,10 +99,10 @@ module "jenkins-gke" {
       name               = "butler-pool"
       #node_count         = 2
       #node_locations     = "us-central1-b,us-central1-c"
-      min_count          = 4
-      max_count          = 4
+      min_count          = 2
+      max_count          = 2
       preemptible        = true
-      machine_type       = "n1-standard-2"
+      machine_type       = "n1-standard-4"
       disk_size_gb       = 50
       disk_type          = "pd-standard"
       image_type         = "COS"
