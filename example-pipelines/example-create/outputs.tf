af23d3c59a4795ece370db26b6abe54e2acbe6bb
gkumar208 <gaurav.kumar@dxc.com>
2021-06-14 06:55:35 +0000

    update op-tgt env

--- example-pipelines/example-create/outputs.tf
+++ example-pipelines/example-create/outputs.tf
@@ -2,6 +2,6 @@ output "example_vpc_name" {
   value = module.gcp-network.network_name
 }
 
-output "example_vm_name" {
-  value = google_compute_instance.vm_0001.name
-}
+# output "example_vm_name" {
+#   value = google_compute_instance.vm_0001.name
+# }
