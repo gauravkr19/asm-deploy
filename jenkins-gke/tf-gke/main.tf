74f3f3a38b93cd5444ba5652fa0cbf7f7c17a2d2
gauravkr19 <gauravkr19@gmail.com>
2021-06-15 06:45:21 +0000

    hub add dep

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -234,7 +234,7 @@ module "asm-jenkins" {
   location         = module.jenkins-gke.location
   cluster_endpoint = module.jenkins-gke.endpoint
   asm_dir          = "asm-dir-${module.jenkins-gke.name}"
-  depends_on       = [module.hub.sa_private_key]
+  depends_on       = [module.hub.sa_private_key, time_sleep.wait_2m]
 }
 
 module "acm-jenkins" {
