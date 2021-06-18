f6fe612cfbbd4cd4160c43d388e301d02a57ea4e
gauravkr19 <gauravkr19@gmail.com>
2021-06-18 10:33:57 +0000

    asm-last try

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -212,7 +212,6 @@ resource "local_file" "cred_file" {
   depends_on = [module.hub]
 }
 
-/*
 module "asm-jenkins" {
   source           = "terraform-google-modules/kubernetes-engine/google//modules/asm"
   version          = "15.0.0"
@@ -238,6 +237,7 @@ module "asm-jenkins" {
   #depends_on           = [local_file.cred_file]
 }
 
+/*
 
 module "acm-jenkins" {
   source           = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/acm"
@@ -320,4 +320,4 @@ resource "null_resource" "istio-comp" {
   depends_on = [kubernetes_namespace.apps-ns, null_resource.deployapps, time_sleep.wait_2m,]
 }
 
- */
+*/
