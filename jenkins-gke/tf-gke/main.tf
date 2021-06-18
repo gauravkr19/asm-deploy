b91a204ec8144b8ceb7ed24811bacef5cbfae875
gauravkr19 <gauravkr19@gmail.com>
2021-06-18 15:08:55 +0000

    add asm-new

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -206,7 +206,6 @@ module "hub" {
   module_depends_on       = var.module_depends_on
 }
 
-/*
 
 resource "local_file" "cred_file" {
   content  = "${base64decode(google_service_account_key.hubsa_credentials.private_key)}"
@@ -238,6 +237,7 @@ module "asm-jenkins" {
   #depends_on           = [local_file.cred_file]
 }
 
+/*
 
 module "acm-jenkins" {
   source           = "github.com/terraform-google-modules/terraform-google-kubernetes-engine//modules/acm"
