8cee9b1a0f601c3a3f67c80ac2fc5955903f23f9
gauravkr19 <gauravkr19@gmail.com>
2021-06-26 14:45:39 +0000

    deploy again

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -113,14 +113,14 @@ module "kubectl-ns" {
   kubectl_destroy_command = "kubectl delete ns asm-system"
 }
 
-# resource "null_resource" "patch-ns" {
-#   depends_on = [module.kubectl-ns]
-#   provisioner "local-exec" {
-#     command = <<EOF
-# kubectl patch ns asm-system --type='json' -p='[{"op": "add", "path": "/metadata/annotations/gke.io~1cluster", "value": "gke://${var.project_id}/${var.region}/${module.jenkins-gke.name}"}]'
-# EOF
-#   }
-# }
+resource "null_resource" "patch-ns" {
+  depends_on = [module.kubectl-ns]
+  provisioner "local-exec" {
+    command = <<EOF
+kubectl patch ns asm-system --type='json' -p='[{"op": "add", "path": "/metadata/annotations/gke.io~1cluster", "value": "gke://${var.project_id}/${var.region}/${module.jenkins-gke.name}"}]'
+EOF
+  }
+}
 
 resource "kubernetes_namespace" "istio" {
   depends_on = [module.jenkins-gke.name] 
